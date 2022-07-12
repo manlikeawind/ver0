f368da8f9e9d605de14e906bb1c2014284660ae7
scott <519076366@qq.com>
2022-07-13 00:24:30 +0800

    init

---
+++ Assets/Script/domain/Base.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+abstract public class BaseObj : MonoBehaviour
+{
+    public virtual void construct(SqliteDataReader reader) { }
+
+    public virtual void fired() { }
+    public virtual void heat() { }
+
+    public virtual void freeze() { }
+    public virtual void cooling() { }
+    public virtual void wind(float dirx, float diry) { }
+
+    public virtual void attacked(int weaponType, float damage) { }
+
+    public virtual void magnetism() { }
+}
---
+++ Assets/Script/domain/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mono.Data.Sqlite;
+
+public class Teleport : BaseObj
+{
+    public string id;
+    public string scene;
+    public float posX;
+    public float posY;
+    public string to;
+
+    public override void construct(SqliteDataReader reader) {
+        reader.Read();
+        this.id = reader.GetString(reader.GetOrdinal("id"));
+        this.scene = reader.GetString(reader.GetOrdinal("scene"));
+        this.posX = reader.GetFloat(reader.GetOrdinal("posX"));
+        this.posY = reader.GetFloat(reader.GetOrdinal("posY"));
+        this.to = reader.GetString(reader.GetOrdinal("to"));
+    }
+}
